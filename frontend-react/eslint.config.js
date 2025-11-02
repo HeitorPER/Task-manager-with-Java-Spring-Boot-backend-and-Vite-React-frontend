0d5e94609242bf45bd0f2f87bae6cad7b957640a
Gabriel Menoni <contato.gmenoni@gmail.com>
2025-11-02 18:39:22 -0300

    Added eslint config

--- frontend-react/eslint.config.js
+++ frontend-react/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', 'build', '*.config.js']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,6 +18,46 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // Regras de qualidade de código
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+
+      // Regras de TypeScript
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+
+      // Regras de React
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': ['warn', {
+        allowConstantExport: true
+      }],
+
+      // Regras de estilo e boas práticas
+      'prefer-const': 'warn',
+      'no-var': 'error',
+      'eqeqeq': ['warn', 'always'],
+      'curly': ['warn', 'all'],
+      'no-trailing-spaces': 'warn',
+      'semi': ['warn', 'never'],
+      'quotes': ['warn', 'single', { avoidEscape: true }],
+      'comma-dangle': ['warn', 'always-multiline'],
+
+      // Regras de organização
+      'no-duplicate-imports': 'warn',
+      'sort-imports': ['warn', {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      }],
     },
   },
 ])
